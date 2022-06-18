43b3c1cc19ba5a087ed70394faba51d975f8c2e5
Ayoush Moktan Lama <aayushlama26@gmail.com>
2022-06-18 17:30:32 +0545

    card

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import cogoToast from 'cogo-toast';
-import './header.css'
-
+import React, { useEffect, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
+import cogoToast from "cogo-toast";
+import "./header.css";
 
 const HeaderWithoutRouter = (props) => {
   const handleChange = (e) => {
     const { value } = e.target;
-    props.onChangeColor(value)
-
-  }
+    props.onChangeColor(value);
+  };
   const [isAcitveHeader, setHeader] = useState(false);
-  const [isMenu, setMenu] = useState(false)
+  const [isMenu, setMenu] = useState(false);
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       if (window.pageYOffset > 32) {
-        setHeader(true)
+        setHeader(true);
       } else {
         setHeader(false);
       }
-    })
+    });
 
-    if(window.innerWidth>=1024){
-      setMenu(true)
+    if (window.innerWidth >= 1024) {
+      setMenu(true);
     }
 
-    window.addEventListener('resize',()=>{
-      if(window.innerWidth>=1024){
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 1024) {
         setMenu(true);
-      }else{
-        setMenu(false)
+      } else {
+        setMenu(false);
       }
-    })
-
-  }, [])
+    });
+  }, []);
   const clickHandler = () => {
-    if (localStorage.getItem('data')) {
-      props.history.push('/dota2RPG')
+    if (localStorage.getItem("data")) {
+      props.history.push("/dota2RPG");
     } else {
-      cogoToast.error('Please sign in first.')
+      cogoToast.error("Please sign in first.");
     }
-  }
+  };
 
-  const lobbyHandler =()=>{
-    if (localStorage.getItem('data')) {
-      props.history.push('/lobbywithbobby')
+  const lobbyHandler = () => {
+    if (localStorage.getItem("data")) {
+      props.history.push("/lobbywithbobby");
     } else {
-      cogoToast.error('Please sign in first.')
+      cogoToast.error("Please sign in first.");
     }
-  }
-
+  };
 
   return (
-
-    <header className={`header animated headroom--not-bottom slideDown ${isAcitveHeader ? 'headroom--not-top slideDown' : 'headroom--top'}`} id="site-header">
+    <header
+      className={`header animated headroom--not-bottom slideDown ${
+        isAcitveHeader ? "headroom--not-top slideDown" : "headroom--top"
+      }`}
+      id="site-header"
+    >
       <div className="container">
         <div className="header-content-wrapper">
           <a href="index-2.html" className="site-logo">
@@ -64,19 +64,41 @@ const HeaderWithoutRouter = (props) => {
 
             <a id="menu-icon-trigger" className="menu-icon-trigger showhide">
               <span className="mob-menu--title">Menu</span>
-              {isMenu? <i onClick={()=>{setMenu(false)}} className="fas fa-times"></i>:<i onClick={()=>{setMenu(true)}} className="fas fa-bars"></i>}
+              {isMenu ? (
+                <i
+                  onClick={() => {
+                    setMenu(false);
+                  }}
+                  className="fas fa-times"
+                ></i>
+              ) : (
+                <i
+                  onClick={() => {
+                    setMenu(true);
+                  }}
+                  className="fas fa-bars"
+                ></i>
+              )}
             </a>
-            <ul className="primary-menu-menu primary-menu-indented scrollable" style={{ maxHeight: '460px', display: `${isMenu?'inline-block':'none'}` }}>
+            <ul
+              className="primary-menu-menu primary-menu-indented scrollable"
+              style={{
+                maxHeight: "460px",
+                display: `${isMenu ? "inline-block" : "none"}`,
+              }}
+            >
               <li>
                 <Link to="/">Home</Link>
               </li>
               <li className="menu-item-has-children">
-                <a className="myNavButton" onClick={lobbyHandler}>Lobby with Bobby</a>
-
+                <a className="myNavButton" onClick={lobbyHandler}>
+                  Lobby with Bobby
+                </a>
               </li>
               <li className="menu-item-has-children">
-                <a className="myNavButton" onClick={clickHandler}>Dota2RPG</a>
-
+                <a className="myNavButton" onClick={clickHandler}>
+                  Dota2RPG
+                </a>
               </li>
               <li className="menu-item-has-mega-menu menu-item-has-children">
                 <Link to="/">Movies</Link>
@@ -89,18 +111,47 @@ const HeaderWithoutRouter = (props) => {
               </li>
             </ul>
           </nav>
-          {!props.profile ? <select onChange={handleChange} className="woox--select language-switcher" data-minimum-results-for-search="Infinity" data-dropdown-css-class="language-switcher-dropdown">
-            <option value="Blue">Bantaba Blue</option>
-            <option value="Green">Bantaba Green</option>
-            <option value="Purple">Bantaba Purple</option>
-          </select> : <>
-            <div className="author-block"><div className="avatar avatar60"><img src={props.user.avatar} alt="avatar" /></div><div className="author-content"><a href="#" className="author-name">{props.user.name}</a><div className="author-prof"><button className='myButton' onClick={() => { localStorage.clear(); props.history.push('/') }}>Logout</button></div></div></div>
-          </>}
-
+          {!props.profile ? (
+            <select
+              onChange={handleChange}
+              className="woox--select language-switcher"
+              data-minimum-results-for-search="Infinity"
+              data-dropdown-css-class="language-switcher-dropdown"
+            >
+              <option value="Blue">Bantaba Blue</option>
+              <option value="Green">Bantaba Green</option>
+              <option value="Purple">Bantaba Purple</option>
+              <option value="Card">Bantaba Card</option>
+            </select>
+          ) : (
+            <>
+              <div className="author-block">
+                <div className="avatar avatar60">
+                  <img src={props.user.avatar} alt="avatar" />
+                </div>
+                <div className="author-content">
+                  <a href="#" className="author-name">
+                    {props.user.name}
+                  </a>
+                  <div className="author-prof">
+                    <button
+                      className="myButton"
+                      onClick={() => {
+                        localStorage.clear();
+                        props.history.push("/");
+                      }}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export const Header = withRouter(HeaderWithoutRouter)
+export const Header = withRouter(HeaderWithoutRouter);
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,89 +1,152 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { About } from '../../components/about/about';
-import { Header } from '../../components/header/header';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { About } from "../../components/about/about";
+import { Header } from "../../components/header/header";
 
 export const Homepage = (props) => {
-    const [imageColor, setImageColor] = useState('Blue')
-    const [isSignIn, setSignIn] = useState(false);
-    const [isLoggedIn, setLogin] = useState(false);
-    const [user, setUser] = useState({});
-    const changeColor = (arg) => {
-        setImageColor(arg)
-        console.log(arg)
-    }
-    const colorUrl = {
-        Blue: 'https://cdn.discordapp.com/attachments/816676404879556621/834804868962058280/BantabaBot.png',
-        Green: 'https://cdn.discordapp.com/attachments/816676404879556621/834805990490767360/BantabaBot2.png',
-        Purple: 'https://cdn.discordapp.com/attachments/816676404879556621/834813188025221161/BantabaBot3.png'
-    }
+  const [imageColor, setImageColor] = useState("Blue");
+  const [isSignIn, setSignIn] = useState(false);
+  const [isLoggedIn, setLogin] = useState(false);
+  const [user, setUser] = useState({});
 
-    useEffect( async() => {
-        
-         async function initialize() {
-            try{
-                if (localStorage.getItem('data')) {
-                    setLogin(true);
-                    setUser(JSON.parse(localStorage.getItem('data')).user)
-        
-                }
-                else{
-                    if (props.location.search.split('=')[0]==='?userId') {
-                    
-                    
-                        const userId = props.location.search.split('=')[1];
-                        const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/user/${userId}`)
-                        if(data){
-                            window.localStorage.setItem('data', JSON.stringify(data));
-                        setLogin(true);
-                        setUser(data.user)
-                        }else{
-                            return
-                        }
-                    }
-                    else{
-                        return
-                    }
-                }
-            }catch(e){
-                console.log(e)
+  let counter = localStorage.getItem("counter");
+
+  const changeColor = (arg) => {
+    if (arg === "Card") {
+      if (!counter) {
+        localStorage.setItem("counter", 0);
+        setImageColor(arg);
+        return;
+      }
+      if (+counter >= 4) {
+        localStorage["counter"] = 0;
+        setImageColor(arg);
+        return;
+      }
+      localStorage["counter"] = +counter + 1;
+    }
+    setImageColor(arg);
+  };
+
+  const CardList = [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/English_pattern_jack_of_spades.svg/360px-English_pattern_jack_of_spades.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/English_pattern_ace_of_diamonds.svg/1200px-English_pattern_ace_of_diamonds.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Atlas_deck_10_of_hearts.svg/1200px-Atlas_deck_10_of_hearts.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/English_pattern_2_of_clubs.svg/1200px-English_pattern_2_of_clubs.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/d/df/Queen_of_Hearts_%28Elizabeth_of_York%29.png",
+  ];
+
+  const colorUrl = {
+    Blue: "https://cdn.discordapp.com/attachments/816676404879556621/834804868962058280/BantabaBot.png",
+    Green:
+      "https://cdn.discordapp.com/attachments/816676404879556621/834805990490767360/BantabaBot2.png",
+    Purple:
+      "https://cdn.discordapp.com/attachments/816676404879556621/834813188025221161/BantabaBot3.png",
+    Card: CardList[+localStorage.getItem("counter")],
+  };
+
+  useEffect(() => {
+    async function initialize() {
+      try {
+        if (localStorage.getItem("data")) {
+          setLogin(true);
+          setUser(JSON.parse(localStorage.getItem("data")).user);
+        } else {
+          if (props.location.search.split("=")[0] === "?userId") {
+            const userId = props.location.search.split("=")[1];
+            const { data } = await axios.get(
+              `${process.env.REACT_APP_BASE_URL}/user/${userId}`
+            );
+            if (data) {
+              window.localStorage.setItem("data", JSON.stringify(data));
+              setLogin(true);
+              setUser(data.user);
+            } else {
+              return;
             }
+          } else {
+            return;
+          }
         }
-        initialize();
-    }, [])
-    
-    
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    initialize();
+  }, [props.location.search]);
 
-    return (<>
-        <Header onChangeColor={changeColor}></Header>
-        <div className="main-content-wrapper">
-            <section data-settings="particles-1" className="main-section crumina-flying-balls particles-js bg-1 medium-padding120 responsive-align-center">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-5 col-md-12 col-sm-12 col-xs-12">
-                            <img className="responsive-width-50" src={colorUrl[imageColor]} alt="image" />
-                        </div>
-                        <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12">
-                            <header className="crumina-module crumina-heading heading--h1 heading--with-decoration">
-                                <h1 className="heading-title f-size-90 weight-normal no-margin">Welcome {isLoggedIn ? user.name : 'Sir'}
-                                </h1>
-                                <h2 className="c-primary">Let's get started.</h2>
-                            </header>
-                            {isSignIn ? <>
-
-                                <a href={`${process.env.REACT_APP_BASE_URL}/auth`} data-scroll className="btn btn--large btn--transparent btn--secondary"><i className="fab fa-discord"></i> &nbsp; Discord</a>
-                                <div style={{ marginTop: 30 }}>
-                                    <a href={`${process.env.REACT_APP_BASE_URL}/auth/steam`} data-scroll className="btn btn--large btn--transparent btn--secondary"><i className="fab fa-steam"></i> &nbsp; Steam</a>
-                                </div>
-
-                            </> : isLoggedIn ? <button onClick={() => { window.localStorage.clear(); setLogin(false); props.history.push('/') }} data-scroll className="btn btn--large btn--transparent btn--secondary">Sign Out</button> : <button onClick={() => { setSignIn(true) }} data-scroll className="btn btn--large btn--transparent btn--secondary">Sign in</button>}
-                        </div>
+  return (
+    <>
+      <Header onChangeColor={changeColor}></Header>
+      <div className="main-content-wrapper">
+        <section
+          data-settings="particles-1"
+          className="main-section crumina-flying-balls particles-js bg-1 medium-padding120 responsive-align-center"
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+                <img
+                  className="responsive-width-50"
+                  src={colorUrl[imageColor]}
+                  alt="image"
+                />
+              </div>
+              <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+                <header className="crumina-module crumina-heading heading--h1 heading--with-decoration">
+                  <h1 className="heading-title f-size-90 weight-normal no-margin">
+                    Welcome {isLoggedIn ? user.name : "Sir"}
+                  </h1>
+                  <h2 className="c-primary">Let's get started.</h2>
+                </header>
+                {isSignIn ? (
+                  <>
+                    <a
+                      href={`${process.env.REACT_APP_BASE_URL}/auth`}
+                      data-scroll
+                      className="btn btn--large btn--transparent btn--secondary"
+                    >
+                      <i className="fab fa-discord"></i> &nbsp; Discord
+                    </a>
+                    <div style={{ marginTop: 30 }}>
+                      <a
+                        href={`${process.env.REACT_APP_BASE_URL}/auth/steam`}
+                        data-scroll
+                        className="btn btn--large btn--transparent btn--secondary"
+                      >
+                        <i className="fab fa-steam"></i> &nbsp; Steam
+                      </a>
                     </div>
-                </div>
-            </section>
-            <About></About>
-        </div>
+                  </>
+                ) : isLoggedIn ? (
+                  <button
+                    onClick={() => {
+                      window.localStorage.clear();
+                      setLogin(false);
+                      props.history.push("/");
+                    }}
+                    data-scroll
+                    className="btn btn--large btn--transparent btn--secondary"
+                  >
+                    Sign Out
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      setSignIn(true);
+                    }}
+                    data-scroll
+                    className="btn btn--large btn--transparent btn--secondary"
+                  >
+                    Sign in
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </section>
+        <About></About>
+      </div>
     </>
-    )
-}
+  );
+};
